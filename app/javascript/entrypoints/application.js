c387d7de28188d018cc12c78997130d762e72719
a <breno.nunesgalvao@protonmail.ch>
2023-10-03 00:34:46 -0300

    add stimulus

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -3,10 +3,12 @@ console.log('Vite ⚡️ Rails')
 import * as Turbo from '@hotwired/turbo'
 Turbo.start()
 
-import ActiveStorage from '@rails/activestorage'
+import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
 
 const channels = import.meta.globEager('./**/*_channel.js')
+
+import "../controllers"
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
