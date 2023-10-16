5494a5d4403e58157cf4f9b6cb1ef108e6263c5d
a <breno.nunesgalvao@protonmail.ch>
2023-10-16 16:00:03 -0300

    fix asset precompile raising on deploy

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,1 +1,3 @@
-RSpotify.authenticate(Rails.application.credentials.spotify_id, Rails.application.credentials.spotify_secret)
+if Rails.application.credentials.spotify_secret
+  RSpotify.authenticate(Rails.application.credentials.spotify_id, Rails.application.credentials.spotify_secret)
+end
